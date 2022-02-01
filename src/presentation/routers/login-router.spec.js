85543b1d2c2beb0d476c1a74e3e4b7ea2f80b348
kleyson castro <kleysoncastro99@gmail.com>
2022-02-01 14:54:12 -0300

    fac: move statsCode for class HttpResquest

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,11 +1,19 @@
+class HttpResponse {
+  static badRequest () {
+    return { statusCode: 400 }
+  }
+
+  static internalServerError () {
+    return { statusCode: 500 }
+  }
+}
+
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest || !httpRequest.body) return { statusCode: 500 }
+    if (!httpRequest || !httpRequest.body) return HttpResponse.internalServerError()
     const { email, password } = httpRequest.body
     if (!email || !password) {
-      return {
-        statusCode: 400
-      }
+      return HttpResponse.badRequest()
     }
   }
 }
