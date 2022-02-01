46c6bce287b486fadcfaf2356cdcf83d77e2e60f
kleyson castro <kleysoncastro99@gmail.com>
2022-02-01 18:20:22 -0300

    ensure return 401 for invalid password or email

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -71,4 +71,17 @@ describe('Login Router', () => {
     expect(authUseCase.email).toBe(httpRequest.body.email)
     expect(authUseCase.password).toBe(httpRequest.body.password)
   })
+
+  test('Should return 401 whwn invalid credential are provider', () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@mail.com',
+        password: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(401)
+  })
 })
