797fbaf7680c6453516aa892e5eac2ef6a32005d
kleyson castro <kleysoncastro99@gmail.com>
2022-02-09 13:35:18 -0300

    Ensure return emaill if call LoadUserEmailRepository

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,6 +1,10 @@
 const { MissingParamError } = require('../../utils/erros')
 
 class AuthUseCase {
+  constructor (loadUserEmailRepository) {
+    this.loadUserEmailRepository = loadUserEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
@@ -8,6 +12,7 @@ class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
+    await this.loadUserEmailRepository.load(email)
   }
 }
 
@@ -22,5 +27,19 @@ describe('Auth useCase', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth('any_email@mail.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  test('Should call LoadUserEmailRepository whith correct email', async () => {
+    class LoadUserEmailRepositorySpy {
+      async load (email) {
+        this.email = email
+      }
+    }
+
+    const loadUserEmailRepositorySpy = new LoadUserEmailRepositorySpy()
+
+    const sut = new AuthUseCase(loadUserEmailRepositorySpy)
+    await sut.auth('any_email@mail.com', 'any_password')
+    expect(loadUserEmailRepositorySpy.email).toBe('any_email@mail.com')
   })
 })
